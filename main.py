26821c200a5300d589d073be43bb27be83c4f7f0
Prabhjot Sodhi <sodhiprabhjot23@gmail.com>
2022-04-05 11:11:27 +1200

    Create Tk Window

---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.geometry('500x300')
+    root.title("Times Table Task")
+    main_frame = tk.Frame(root)
+    main_frame.pack()
+    root.mainloop()
