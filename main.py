359d11bb14d92ca859e81ec4a22e7a10d1eaed99
Prabhjot Sodhi <sodhiprabhjot23@gmail.com>
2022-04-06 13:30:38 +1200

    Backend Init

--- main.py
+++ main.py
@@ -4,15 +4,26 @@ class TimesTableGUI:
     def __init__(self, frame):
         self.frame = frame
         self.display()
+        backend = TimeTableClass()
     def display(self):
         question = tk.Label(self.frame, text="9 * 3")
         question.grid()
         answer = tk.Entry(self.frame)
         answer.grid(column=1, row=0)
-        check_answer_box = tk.Button(self.frame, text="Check Answer")
+        check_answer_box = tk.Button(self.frame, text="Check Answer" command=lambda: backend.check_answer(question))
         check_answer_box.grid(row=1, column=0)
         next_question_box = tk.Button(self.frame, text="Next Question")
         next_question_box.grid(row=1, column=1)
+        check_label = tk.Label(self.frame, text="")
+        check_label.grid(row=2, column=0)
+
+class TimeTableClass:
+    def __init__(self):
+        pass
+    def check_answer(self, input):
+        input = input.get()
+
+
 
 if __name__ == '__main__':
     root = tk.Tk()
